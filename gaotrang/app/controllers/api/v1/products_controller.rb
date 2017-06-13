43c10471a9cf7eff5ee73310b56603a80829e467
Anh Du Lio <lio.anhdu@gmail.com>
2017-06-13 08:48:03 +0700

    fix find by friendly-id

--- gaotrang/app/controllers/api/v1/products_controller.rb
+++ gaotrang/app/controllers/api/v1/products_controller.rb
@@ -44,7 +44,7 @@ module Api::V1
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_product
-        @product = Product.find(params[:id])
+        @product = Product.friendly_id.find(params[:id])
       end
 
       # Only allow a trusted parameter "white list" through.
